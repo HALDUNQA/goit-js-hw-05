2e269f05bde9c25e72fb87718be2be691f731fb2
Haldun <cetinkanat@gmail.com>
2025-06-11 10:51:35 -0400

    task 3 complet

--- js/task-4.js
+++ js/task-4.js
@@ -1,49 +1,45 @@
-
-
- const getTotalBalanceByGender = (users, gender) => {
-
-  return users.reduce ( (total, user) => {
+const getTotalBalanceByGender = (users, gender) => {
+  return users.reduce((total, user) => {
     return user.gender === gender ? total + user.balance : total;
-  }, 
-  0);
+  }, 0);
 };
 
 const allUsers1 = [
-	{
+  {
     name: "Moore Hensley",
     gender: "male",
-    balance: 2811
+    balance: 2811,
   },
   {
     name: "Sharlene Bush",
     gender: "female",
-    balance: 3821
+    balance: 3821,
   },
   {
     name: "Ross Vazquez",
     gender: "male",
-    balance: 3793
+    balance: 3793,
   },
   {
     name: "Elma Head",
     gender: "female",
-    balance: 2278
+    balance: 2278,
   },
   {
     name: "Carey Barr",
     gender: "male",
-    balance: 3951
+    balance: 3951,
   },
   {
     name: "Blackburn Dotson",
     gender: "male",
-    balance: 1498
+    balance: 1498,
   },
   {
     name: "Sheree Anthony",
     gender: "female",
-    balance: 2764
-  }
+    balance: 2764,
+  },
 ];
 
 console.log(getTotalBalanceByGender(allUsers1, "male")); // 12053
